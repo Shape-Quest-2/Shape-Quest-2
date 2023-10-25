c5d596c0d5518e2d3d52fe94cbe125c5aa88f0f9
Tyler Shelton <tdshelton@gmail.com>
2023-10-25 10:22:17 -0700

    Make calls that return a response to the client `return`

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,11 +3,11 @@ const router  = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/SignUpForm', userController.signUp, (req, res) => {
-  res.status(201).json({ message: 'Signed in succesfully!', redirect: '/' });
+  return res.status(201).json({ message: 'Signed in succesfully!', redirect: '/' });
 })
 
 router.post('/', userController.login, (req, res) => {
-  res.status(200).json({ message: 'Login successful', redirect: '/GameContainer'})
+  return res.status(200).json({ message: 'Login successful', redirect: '/GameContainer'})
 })
 
 module.exports = router;
