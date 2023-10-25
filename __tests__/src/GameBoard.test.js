1645379537aa74aea79c23f431850a803ab527b5
Matin Schams <matinschams@Matins-MacBook-Pro.local>
2023-10-25 10:55:57 -0700

    Partial FrontEnd Testing

---
+++ __tests__/src/GameBoard.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react'
+import GameBoard from '../../src/components/GameBoard';
+
+describe ('GameBoard Component', () => {
+    
+});
