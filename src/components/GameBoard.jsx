5d4c566c4a19303c334c139f22406a90eb99309e
zackweiss <zweiss1881@gmail.com>
2023-10-26 16:19:37 -0400

    working drag and drop

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import '/src/sass/styles.css';
+import { useDroppable } from '@dnd-kit/core';
+import Shape from './Shape';
 
 const GameBoard = ({ shapes, handleChangeExistingShapeColor }) => {
+  const { isOver, setNodeRef } = useDroppable({
+    id: 'droppable',
+  });
+  const style = {
+    color: isOver ? 'green' : undefined,
+  };
+
   const renderShapes = shapes.map((element, index) => (
-    <div
-      onClick={e => {
-        handleChangeExistingShapeColor(e.target.dataset.id);
-      }}
-      style={{ backgroundColor: element.color }}
-      id={`${element.shape}1`}
-      data-id={index}
-      key={index}>
-      {element.shape}
-    </div>
+    <Shape
+      shapes={shapes}
+      element={element}
+      index={index}
+      handleChangeExistingShapeColor={handleChangeExistingShapeColor}
+    />
   ));
 
-  return <div className='game-board'>{renderShapes}</div>;
+  return (
+    <div ref={setNodeRef} style={{ ...style }} className='game-board'>
+      {renderShapes}
+    </div>
+  );
 };
 
 export default GameBoard;
--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ColorTools from './ColorTools';
 import ShapeTools from './ShapeTools';
 
