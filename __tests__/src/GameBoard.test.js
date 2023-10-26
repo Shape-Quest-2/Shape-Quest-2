728f2dc8f3c15c2a0d6576a9e7f69aae34b956e7
Matin Schams <matinschams@Matins-MacBook-Pro.local>
2023-10-26 10:04:37 -0700

    Complete Gameboard.test.js

--- __tests__/src/GameBoard.test.js
+++ __tests__/src/GameBoard.test.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import GameBoard from '../../src/components/GameBoard';
 import '@testing-library/jest-dom'
 
 describe ('GameBoard Component', () => {
-    test('should render the shape with selected color', () => {
-        const { getByText } = render(<GameBoard selectedShape="circle" selectedColor="blue" />);
-        const circleElement = getByText('circle');
-        expect(circleElement).toHaveStyle('background-color: blue');
+  let mockFunction;
+  beforeAll(() => {
+    mockFunction = jest.fn();
+  });
+    // test('should render the shape with selected color', () => {
+    //     const { getByText, rerender} = render(<GameBoard shapes = {[{ shape: 'circle', color: 'blue' }]} selectedShape="circle" selectedColor="blue" />);
+    //     const circleElement = getByText('circle');
+    //     expect(circleElement).toHaveStyle('background-color: blue');
+    //     expect(circleElement).toBeInTheDocument();
+    //     rerender( <GameBoard shapes={[{ shape: 'oval', color: 'green' }]} selectedShape="oval" selectedColor="green"/>);
+        
+    //     const ovalElement = getByText('oval');
+    //     expect(ovalElement).toBeInTheDocument();
+    //     expect(squareElement).toBeInTheDocument();
+    //   });
+    test('should render shapes with their colors and handle shape color change on click', () => {      
+      const { getByText } = render(<GameBoard shapes={[{ shape: 'circle', color: 'blue' }, { shape: 'square', color: 'red' }]} handleChangeExistingShapeColor={mockFunction} />);
+      
+      const circleElement = getByText('circle');
+      const squareElement = getByText('square');
+        
+      expect(circleElement).toHaveStyle('background-color: blue');
+      expect(squareElement).toHaveStyle('background-color: red');
+      
+      fireEvent.click(circleElement);
+      
+      expect(mockFunction).toHaveBeenCalledWith('0');
       });
       
-      test('should add shapes to board when selectedShape changes', () => {
-        const { getByText, rerender } = render(<GameBoard selectedShape="square" selectedColor="red" />);
-        const squareElement = getByText('square');
-        expect(squareElement).toBeInTheDocument();
-        rerender(<GameBoard selectedShape="oval" selectedColor="green" />);
-        const ovalElement = getByText('oval');
-        expect(ovalElement).toBeInTheDocument();
-        expect(squareElement).toBeInTheDocument();
-      });
     });
