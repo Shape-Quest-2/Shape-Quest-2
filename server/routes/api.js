df38d0c090e4fed06c0897a711e51b8bbd79440f
Tyler Shelton <tdshelton@gmail.com>
2023-10-26 08:53:56 -0700

    Add semis

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,10 +4,8 @@ const userController = require('../controllers/userController');
 
 router.post('/SignUpForm', userController.signUp, (req, res) => {
   return res.status(201).json({ redirect: '/' });
-})
+});
 
-router.post('/', userController.login, (req, res) => {
-  return res.status(200).json({ redirect: '/GameContainer' })
-})
+});
 
 module.exports = router;
