fb088f9b9e9c98b4b23cb0fd296742674b4c2977
Tyler Shelton <tdshelton@gmail.com>
2023-10-26 15:18:51 -0700

    Ensure routes return intended json

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,24 +1,14 @@
 const express = require('express');
-const router = express.Router();
+const router  = express.Router();
 const userController = require('../controllers/userController');
 
+router.post('/SignUpForm', userController.signUp, (req, res) => {
+  return res.status(200).json({ redirect: '/' });
+});
 
-// router.post('/SignUpForm', userController.signUp, (req, res) => {
-//   // console.log('signedup!')
-//   res.sendStatus(200)
-// })
-
-router.post('/SignUpForm', (req, res) => {
-  // console.log('signedup!')
-  res.sendStatus(200)
+router.post('/', userController.login, (req, res) => {
+  return res.status(200).json({ redirect: '/GameContainer'})
 })
-
-// router.post('/', userController.login, (req, res) => {
-//   //redirect to home page
-//   // console.log('Login successful!')
-//   // res.redirect('/hi')
-//   res.status(200).json({ redirect: '/GameContainer'})
-// })
 
 
 module.exports = router;
