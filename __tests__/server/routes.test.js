51a0848c011cda4facb827af18712ffdf8886647
Tyler Shelton <tdshelton@gmail.com>
2023-10-26 15:19:48 -0700

    Working supertest...test

--- __tests__/server/routes.test.js
+++ __tests__/server/routes.test.js
@@ -1,32 +1,13 @@
 const request = require('supertest');
-const express = require('express')
-const app = express()
-const router = require('../../server/routes/api.js')
-const server = require('../../server/server.js');
-
-
-app.use('/', router)
-afterAll((done) => {
-    // Close the server to prevent open handles
-    server.close((err) => {
-      if (err) {
-        console.error('Error closing the server:', err);
-      } else {
-        console.log('Server closed.');
-      }
-      done();
-    });
-  });
+const server = 'http://localhost:3000';
+  
 describe('Route integration testing', () => {
     it('should respond with status 200 in /SignUpForm', async () => {
-        const response = await request(app)
+      const response = await request(server)
         .post('/SignUpForm')
         .send({ username: 'testUser', password: 'testpass' });
-        console.log(response)
 
-        expect(response.status).toEqual(200);
-
-        expect(response).toEqual('username')
-
-    })
+      expect(response.status).toEqual(200);
+      expect(response.body.redirect).toEqual('/');
+    });
 });
